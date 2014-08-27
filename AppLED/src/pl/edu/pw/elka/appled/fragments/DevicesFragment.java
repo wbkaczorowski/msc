4cf3633ed49cbf5f76485f25677ea4a74941ca3e
Wojciech Kaczorowski <wbkaczorowski@gmail.com>
2014-08-28 00:50:32 +0200

    Communication v.0.0.1 done.

---
+++ AppLED/src/pl/edu/pw/elka/appled/fragments/DevicesFragment.java
@@ -0,0 +1,55 @@
+package pl.edu.pw.elka.appled.fragments;
+
+import pl.edu.pw.elka.appled.R;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * 
+ * @author Wojciech Kaczorowski
+ *
+ */
+public class DevicesFragment extends Fragment {
+
+    private Button allDevicesButton;
+    private Button noDevicesButton;
+    
+    
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.devices_fragment, container, false);
+        allDevicesButton = (Button) rootView.findViewById(R.id.all_devices_button);
+        noDevicesButton = (Button) rootView.findViewById(R.id.no_devices_button);
+        
+        allDevicesButton.setOnClickListener(new OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+                // TODO zaznaczanie wszystkiego na liście
+            }
+        });
+        
+        noDevicesButton.setOnClickListener(new OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+                // TODO czyszczenie listy
+            }
+        });
+        
+        
+        
+        return rootView;
+    }
+
+    
+    
+    
+    
+    
+}
