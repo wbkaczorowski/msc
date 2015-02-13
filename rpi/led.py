93d4829c32d7d080d224a0e4aa3dce16f550a6c1
Wojciech Kaczorowski <wbkaczorowski@gmail.com>
2015-02-14 00:30:50 +0100

    Controll of LEDs done in right way. (changed library)

--- rpi/led.py
+++ rpi/led.py
@@ -1,23 +1,30 @@
-# from RPIO import PWM
+import RPi.GPIO as GPIO
 
-# TODO rozjebane w bizde to to
-# inne pwm obczaic?
 
 class LEDController(object):
     RED_PIN = 23
     GREEN_PIN = 24
     BLUE_PIN = 25
+    PWM_FREQ = 50 # in Hz
 
     def __init__(self):
-        self.current_RGB = "ffaacc"
+        self.current_RGB = "000000"
         self.red = 0
         self.green = 0
         self.blue = 0
-        # self.servo = PWM.Servo()
 
-        # PWM.setup()
-        # PWM.init_channel(3, 255 * 30)
-        # PWM.init_channel(4, 255*20)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.RED_PIN, GPIO.OUT)
+        GPIO.setup(self.GREEN_PIN, GPIO.OUT)
+        GPIO.setup(self.BLUE_PIN, GPIO.OUT)
+
+        self.pwm_red = GPIO.PWM(self.RED_PIN, self.PWM_FREQ)
+        self.pwm_green = GPIO.PWM(self.GREEN_PIN, self.PWM_FREQ)
+        self.pwm_blue = GPIO.PWM(self.BLUE_PIN, self.PWM_FREQ)
+
+        self.pwms = [self.pwm_red, self.pwm_green, self.pwm_blue]
+        for pwm in self.pwms:
+            pwm.start(0)
 
 
     def update_rgb(self, rgb_string_value):
@@ -25,19 +32,14 @@ class LEDController(object):
         self.green = self.get_green_value(rgb_string_value)
         self.blue = self.get_blue_value(rgb_string_value)
         self.current_RGB = self.rgb_hex_string()
-
         self.update_red()
-        # self.update_green()
-        # self.update_blue()
+        self.update_green()
+        self.update_blue()
 
     def stop(self):
-        # self.servo.stop_servo(self.RED_PIN)
-        # self.servo.stop_servo(self.GREEN_PIN)
-        # self.servo.stop_servo(self.BLUE_PIN)
-
-        # PWM.clear_channel_gpio(3, self.RED_PIN)
-        # PWM.clear_channel_gpio(4, self.GREEN_PIN)
-        # PWM.cleanup()
+        for pwm in self.pwms:
+            pwm.stop()
+        GPIO.cleanup()
         print "LEDs stoped"
 
 
@@ -47,26 +49,22 @@ class LEDController(object):
 
     # TODO te wartosci poprawic
     def update_red(self):
-        print "red: ", self.red
-
-        # PWM.add_channel_pulse(3, self.RED_PIN, 0, self.red * 2)
-        # self.servo.set_servo(self.RED_PIN, round(self.red * 78, -1))
-
-        print "red: ", round(self.red * 78, -1)
+        # print "red: ", self.red
+        # print "red: ", self.red*100.0/255.0
+        self.pwm_red.ChangeDutyCycle(self.red*100.0/255.0)
 
     def update_green(self):
-        # PWM.add_channel_pulse(4, self.GREEN_PIN, 0, self.green*2)
-        print "green: ",  self.green
+        # print "green: ",  self.green
+        # print "green: ",  self.green*100.0/255.0
+        self.pwm_green.ChangeDutyCycle(self.green*100.0/255.0)
 
-        # self.servo.set_servo(self.GREEN_PIN, round(self.green * 78, -1))
-        print "green: ",  round(self.green * 78, -1)
 
     def update_blue(self):
-        self.servo.set_servo(self.BLUE_PIN, round(self.blue * 78, -1))
-        print "blue: ", round(self.blue * 78, -1)
+        # print "blue: ",  self.blue
+        # print "blue: ",  self.blue*100.0/255.0
+        self.pwm_blue.ChangeDutyCycle(self.blue*100.0/255.0)
 
 
-    # TODO sprawdzic czy dobrze zczytuje miejsca w stringu
     @staticmethod
     def get_red_value(data):
         return int(data[0:2], 16)
